88c92f76baf22161fa54d8d17622afb36f0bf86c
futa <kick20000@gmail.com>
2025-01-26 21:01:10 +0900

    refactor: ヘッダーとフッターを別コンポーネントに分離

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="bg-gradient-to-r from-blue-500 to-blue-600 shadow-lg py-6">
+      <h1 className="text-3xl font-bold text-center text-white tracking-wide font-mplus">
+        適当に相槌チャット
+      </h1>
+    </header>
+  );
+}
